59ec612217397de959ca63f52b4083468972c332
Jinsu Kim <jinsukim@ip-192-168-45-174.ap-northeast-2.compute.internal>
2024-02-29 00:29:07 +0900

    first commit

---
+++ zeppelin/build.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+pip install -r requirements.txt
+
+wget https://archive.apache.org/dist/spark/spark-3.2.4/spark-3.2.4-bin-hadoop2.7.tgz
+
+wget https://downloads.lightbend.com/scala/2.12.18/scala-2.12.18.tgz
+
+tar zxvf spark-3.2.4-bin-hadoop2.7.tgz
+
+tar zxvf scala-2.12.18.tgz
+
+rm spark-3.2.4-bin-hadoop2.7.tgz
+
+rm scala-2.12.18.tgz
+
+mv spark-3.2.4-bin-hadoop2.7 spark
+
+mv scala-2.12.18 scala
