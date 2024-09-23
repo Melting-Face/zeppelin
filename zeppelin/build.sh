d8c900a1a53d799053b08da5842a418b08be487d
jin.js <jin.js@jinjsui-MacBookPro.local>
2024-09-23 15:49:27 +0900

    modify docker file

--- zeppelin/build.sh
+++ zeppelin/build.sh
@@ -2,18 +2,18 @@
 
 pip install -r requirements.txt
 
-wget https://archive.apache.org/dist/spark/spark-3.2.4/spark-3.2.4-bin-hadoop2.7.tgz
+wget https://archive.apache.org/dist/spark/spark-3.2.2/spark-3.2.2-bin-hadoop2.7.tgz
 
 wget https://downloads.lightbend.com/scala/2.12.18/scala-2.12.18.tgz
 
-tar zxvf spark-3.2.4-bin-hadoop2.7.tgz
+tar zxvf spark-3.2.2-bin-hadoop2.7.tgz
 
 tar zxvf scala-2.12.18.tgz
 
-rm spark-3.2.4-bin-hadoop2.7.tgz
+rm spark-3.2.2-bin-hadoop2.7.tgz
 
 rm scala-2.12.18.tgz
 
-mv spark-3.2.4-bin-hadoop2.7 spark
+mv spark-3.2.2-bin-hadoop2.7 spark
 
 mv scala-2.12.18 scala
